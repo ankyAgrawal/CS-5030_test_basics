f0054d2d9bb3c6c6ebd37a5c9c5904c3d3bdaa7b
Unnam29 <113061292+Unnam29@users.noreply.github.com>
2022-11-06 20:21:17 -0600

    Update todo.service.js

--- test-api-js/todo.service.js
+++ test-api-js/todo.service.js
@@ -23,11 +23,29 @@ class todoservice{
     }
 
     add_todo(todo){
-        // Your code here
+        const { id, item, completed } = req.body;
+    const newTodo = {
+      id,
+      item,
+      completed,
+    };
+    todos.push(newTodo);
+    return res.status(201).json({
+      data: todos,
+      error: null,
+    });
     }
 
     delete_todo(id){
         // Your code here
+        const id = req.params.id
+    const todo = todos[0]
+    if(todo) {
+      todos.splice(id, 1)
+    }
+    return res.status(200).json({
+      data: todos,
+      error: null,
     }
 
     update_todo(id, todo){
